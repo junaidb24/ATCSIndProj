63ae5b3f8cade2230122d3c9bb8fa935e396519d
Junaid Bhatti <jbhatti24@students.polytechnic.org>
2024-05-15 12:13:15 -0700

    wednesday work

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ATCSIndProj
 Final project for 2024 advanced topics in cs course at Polytechnic
+This project was created by Junaid Bhatti Class of 2024
+
+In the past few months I have occasionally been playing online poker. (WITH FAKE MONEY)
+This inclined me to attempt to create a poker game for my final independent project.
+I started off with making a layout of the things that I thought i would need. These are the 4 classes, Card, Deck, Hand, and Game
--- poker.py
+++ poker.py
@@ -9,6 +9,8 @@ trying to create a poker class
 __author__ = "Junaid Bhatti"
 __version__ = "2024-02-01"
 
+import random
+
 class Card:
     def __init__(self, suit, value):
         self.suit = suit
@@ -18,18 +20,28 @@ class Card:
 class Deck:
     def __init__(self):
         self.cards = [] #list to store the cards
+        #need to create a standardized deck of 52 playing cards
+        #seperating them between numbered cards and face cards
         for suit in ['Spades', 'Clubs', 'Hearts', 'Diamonds']: #creating numbered cards between 2-10 and putting them in the deck
             for value in range[2,11]:
                 self.cards.append(Card(suit, str(value)))
             for rank in ['Jack', 'Queen', 'King', 'Ace']: #creating face cards and adding them to deck
+                self.cards.append(Card(suit,rank))
 
     def deal_cards(self):
-    
+        #if the deck is not empty, deal one from the top of the deck
+        #if its empty return none + printstatement, if not return the card
+        if len(self.cards) > 0:
+            return self.cards.pop(0)
+        else:
+            return None + "There are no more cards, you need to reshuffle"
+        
     def shuffle(self):
-
+        #randomly shuffling the cards
+        random.shuffle(self.cards)
 
 class Hand:
-    def printhand
+    def 
 
 class Game:
 
